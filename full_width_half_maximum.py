18bdee038a560bfad5e1aab5887fb485d2a61c47
Arthur Pyuskulyan <103001416+arthurpyuskulyan@users.noreply.github.com>
2025-01-12 00:48:23 -0800

    Add files via upload

---
+++ full_width_half_maximum.py
@@ -0,0 +1,21 @@
+#full width half max
+def calculate_fwhm(energies, intensities):
+    max_intensity = max(intensities)
+    half_max = max_intensity / 2
+
+    left_idx = None
+    right_idx = None
+
+    for i, intensity in enumerate(intensities):
+        if intensity >= half_max:
+            left_idx = i
+            break
+
+    for i in range(len(intensities) - 1, -1, -1):
+        if intensities[i] >= half_max:
+            right_idx = i
+            break
+            
+
+    fwhm = energies[right_idx] - energies[left_idx]
+    return fwhm
