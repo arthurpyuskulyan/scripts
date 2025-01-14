5564f12a83b46a49779252a64e21d989f0a72ce0
Arthur Pyuskulyan <103001416+arthurpyuskulyan@users.noreply.github.com>
2025-01-13 21:55:20 -0800

    Update atomic_distances.py

--- atomic_distances.py
+++ atomic_distances.py
@@ -1,12 +1,13 @@
+# The interatomic distances are computed using MDAnalysis
 import os
 import MDAnalysis as mda
 frameanddistancen10c4 = []
-for i in range(1, 3):  # Assumes you have f1.xyz to f5.xyz
+for i in range(1, 3):  
     file_path = os.path.join('xyzfiles/', f'f{i}.xyz')
     u = mda.Universe(file_path)
-    selected_atoms = u.atoms[:32]
-    n10 = selected_atoms[10]  # Assuming atom with index 0
-    c4 = selected_atoms[4]  # Assuming atom with index 1
-    n10c4distance = mda.lib.distances.distance_array(n10.position, c4.position)[0][0]
+    selected_atoms = u.atoms[:32] # This selects so only the first 32 atoms are considered, which was the case for cresyl violet dye with 32 atoms
+    n10 = selected_atoms[10]  # Atoms start with index 0
+    c4 = selected_atoms[4]  
+    n10c4distance = mda.lib.distances.distance_array(n10.position, c4.position)[0][0] # Computes distances between nitrogen and carbon
     frameanddistancen10c4.append((i, n10c4distance))
 print(frameanddistancen10c4)
