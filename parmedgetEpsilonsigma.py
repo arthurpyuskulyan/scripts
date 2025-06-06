b58a763d522ea780f028a3d1e93c0e5ef22825c4
Arthur Pyuskulyan <103001416+arthurpyuskulyan@users.noreply.github.com>
2025-06-06 00:27:33 -0700

    Delete parmedgetEpsilonsigma.py

--- parmedgetEpsilonsigma.py
+++
@@ -1,6 +0,0 @@
-#This is a script that uses parmed to get the epsilon and sigma for each atom
-import parmed
-parm = parmed.load_file('cvmethanol.prmtop')
-for atom in parm.atoms:
-    print(atom.type, atom.rmin, atom.epsilon)
-
